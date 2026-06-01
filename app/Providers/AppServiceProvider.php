eb7a93ef62f999ba778fa36e318c2834bbec592c
yopihendriansah90 <yopihendriansah90@gmail.com>
2026-06-01 20:23:48 +0700

    superadmin full akses

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::before(function (?User $user, string $ability): ?bool {
+            return $user?->hasRole('super_admin') ? true : null;
+        });
     }
 }

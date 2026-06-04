83536ad84b7fda052a0d120aae233340b6a16189
yopihendriansah90 <yopihendriansah90@gmail.com>
2026-06-04 14:29:01 +0700

    update route gate

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\GateAuthController;
 use App\Http\Controllers\StudentTicketQrDownloadController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/gate');
 
 Route::get('/students/{student}/ticket-qr.jpg', StudentTicketQrDownloadController::class)
     ->name('students.ticket-qr.download');
